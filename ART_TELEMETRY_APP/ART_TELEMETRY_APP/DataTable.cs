a6a53df040ab6bbb1ce0681e0457b9395f1d66e8
Bólya Richárd <44650681+bricsi0000000000000@users.noreply.github.com>
2020-01-24 08:06:57 +0100

    telemetry_app

---
+++ ART_TELEMETRY_APP/ART_TELEMETRY_APP/DataTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using LiveCharts;
+
+namespace ART_TELEMETRY_APP
+{
+    class DataTable
+    {
+        List<Tuple<string, ChartValues<float>>> datas = new List<Tuple<string, ChartValues<float>>>();
+
+        public DataTable(string attribute, ChartValues<float> entities)
+        {
+            datas.Add(new Tuple<string, ChartValues<float>>(attribute, entities));
+        }
+
+        public List<Tuple<string, ChartValues<float>>> Datas { get => datas; set => datas = value; }
+
+        public ChartValues<float> getFiltered
+        {
+            get
+            {
+                ChartValues<float> filtered = new ChartValues<float>(datas[0].Item2);
+
+                int total = filtered.Count;
+                Random rand = new Random(DateTime.Now.Millisecond);
+                while (filtered.Count / (float)total > .004)
+                {
+                    filtered.RemoveAt(rand.Next(1, filtered.Count - 1));
+                }
+
+                return filtered;
+            }
+        }
+    }
+}
---
+++ ART_TELEMETRY_APP/ART_TELEMETRY_APP/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Charts;
+using LiveCharts.Wpf;
+
+namespace ART_TELEMETRY_APP
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void importFileBtnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open_file_dialog = new OpenFileDialog();
+            DataReader data_reader = new DataReader();
+            if (open_file_dialog.ShowDialog() == true)
+            {
+                data_reader = new DataReader(open_file_dialog.FileName);
+            }
+
+            ChartBuilder chart_builder = new ChartBuilder();
+            chart_builder.AddGroup("group_a");
+            chart_builder.AddChartToGroup("group_a", "chart_a", data_reader.DataTables[0].getFiltered);
+            chart_builder.BuildGroup("group_a", grid);
+            chart_builder.BuildChart("group_a", "chart_a");
+          
+        }
+    }
+}
